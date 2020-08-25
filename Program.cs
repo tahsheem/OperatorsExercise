0a9f4ef33a43c10b11c0a1f76c4e19d7975e061a
mo ali <tahsheem24@gmail.com>
2020-08-24 19:05:01 -0700

    This Commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //------EXERCISE 1 ----------
+            //17/4 is a 4 remainder 1
+
+            var a = 17;
+            var b = 4;
+
+            var div = a / b;
+            var mod = a % b;
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod} ");
+
+
+
+            //----------EXERCISE 2-------------
+
+
+          
+
+
+            //-----EXERCISE 3 --------------
+
+
+            //ALLOWS USER TO INPUT THE RADIUS
+
+            Console.WriteLine("Please enter a number to find the PI?");
+            var radius = double.Parse(Console.ReadLine());
+
+
+            //CALCULATE AREA
+            var areaOfCircle = CalculateArea(radius);
+
+            Console.WriteLine($"The area of a Circle with radius of {radius} is {areaOfCircle}");
+           
+            
+        }
+           public static double CalculateArea( double radius)
+        {
+            return Math.PI * (radius * radius);
+
+        }
+        }
+
+
+            
+    }
